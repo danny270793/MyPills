8a1523780f4427d7ba16d4b6debf04f23070c6fd
Danny Vaca <danny270793@icloud.com>
2026-07-09 18:12:31 -0500

    Initial Commit

---
+++ MyPills/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPills
+//
+//  Created by Danny Vaca on 9/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPills/MyPillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPillsApp.swift
+//  MyPills
+//
+//  Created by Danny Vaca on 9/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MyPillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
